c4b02202694f7ad5a9d0ad973733cf2664f517cc
BluDobson <bradleydobson11@hotmail.co.uk>
2021-06-14 11:14:17 +0000

    changed app1.py

--- myproject/app1.py
+++ myproject/app1.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, url_for, redirect
 
 app = Flask(__name__)
 
@@ -11,10 +11,17 @@ def home():
 def about():
     return 'This is the about page'
 
+@app.route('/square')
+def square_home():
+    return 'Enter a number above 1 at the end of the url'
+
 @app.route('/square/<int:number>')
 def square(number):
-    ans = number*number
-    return str(ans)
+    if number >= 1:
+        ans = number*number
+        return str(ans)
+    else:
+        return redirect(url_for('square_home'))
 
 if __name__ == "__main__":
     app.run(debug=True, host = '0.0.0.0')
