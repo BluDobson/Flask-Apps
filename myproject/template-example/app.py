faf3c4ca13715a3bcffec7b031f77b8a6ca8c9aa
BluDobson <bradleydobson11@hotmail.co.uk>
2021-06-15 15:16:38 +0000

    added template-example app

---
+++ myproject/template-example/app.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/ben')
+def ben():
+    return render_template('ben.html')
+
+@app.route('/harry')
+def harry():
+    return render_template('harry.html')
+
+@app.route('/list')
+def list_():
+    return render_template('list.html', users=["ben", "harry", "bob", "jay", "matt", "bill"])
+
+if __name__ == "__main__":
+    app.run(debug=True, host='0.0.0.0')
