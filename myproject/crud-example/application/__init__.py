556cd3a893d9811f21e5118c842a5e2ec2f35461
BluDobson <bradleydobson11@hotmail.co.uk>
2021-06-15 11:41:51 +0000

    edited crud-example app

--- myproject/crud-example/application/__init__.py
+++ myproject/crud-example/application/__init__.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://root:happypridemonth@35.189.72.151/games" #enter info root:pwd@localhost/db
+app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql:///games" #enter info root:pwd@localhost/db
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy(app)
 
